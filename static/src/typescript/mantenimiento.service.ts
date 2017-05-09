4261c4895513aea461f22226125a7b89e2012c5e
BryanMauriz <bryanmauriz@gmail.com>
2017-05-08 19:23:39 -0500

    sistema actualizado 08/05/17

--- static/src/typescript/mantenimiento.service.ts
+++ static/src/typescript/mantenimiento.service.ts
@@ -2,7 +2,7 @@ declare var BASEURL : string;
 
 export class MantenimientoSistemaService{
     
-    private url_mantenimientoService : string = 'http://192.168.200.123:8080/apirest/sistemas/';
+    private url_mantenimientoService : string = 'http://192.168.200.123:82/apirest/sistemas/';
     
     getSistemas(): JQueryXHR{
         return $.ajax({
@@ -16,5 +16,18 @@ export class MantenimientoSistemaService{
             type: 'POST',
             data: data
         })
+    }
+    editSistema(pk:number, data: Object): JQueryXHR{ 
+        return $.ajax({
+            url: `${this.url_mantenimientoService}${pk}/`,            
+            type: 'PUT',
+            data: data
+        })
+    }
+    deleteSistema(pk: number): JQueryXHR {
+        return $.ajax({
+            url: `${this.url_mantenimientoService}${pk}/`,
+            type: 'DELETE',
+        });
     }
 }
--- static/src/typescript/mantenimientoView.ts
+++ static/src/typescript/mantenimientoView.ts
@@ -7,61 +7,94 @@ export class modulo_sistema{
 
     constructor(){
 
+       $('.table').on('click', '.botonsaveEditar', (ev) =>{                   
+           let aux = ev.currentTarget
+           let value = $(aux).data("value")
+           this.geteditarSistemas(value)   
+       });
+
+       $('.table').on('click', '.eliminarSistema', (ev) =>{                   
+           let aux = ev.currentTarget
+           let value = $(aux).data("value")
+           this.geteliminarSistema(value)   
+       });
+
         this.getobtenerSistemas();
 
-        $('#save_sistema').on('click', () => {
-             
-            this.setguardarSistemas();                   
-        });
-
-       
-        
+        $('#newSystem').on('click', '#save_sistema',(ev) => {
+            let aux = ev.currentTarget
+           let value =  $(aux).data("value")
+           let  nombre = $("#nom_sistema").val()
+           let descripcion =  $("#descripcion_sistema").val()
+           console.log( $("#nom_sistema").val())
+            this.setguardarSistemas(value,nombre,descripcion);   
+        });   
+      
     }
+
+    
 
     getobtenerSistemas(){
         this.mantenimientosistemaService.getSistemas().done((data)=>{            
             for(let entry of data){                
                 let aux =`<tr><td>---</td><td>${entry.id}</td><td>${entry.nombre}</td><td>${entry.descripcion}</td><td>---</td><td><ul class="icons-list">
-                <li class="text-primary-600" data-toggle="modal" data-target="#newSystem"><a name="sit" data-value="${entry.id}"><i  class="icon-pencil7" >
-                </i></a></li><li class="text-danger-600"><a><i class="icon-trash"></i></a></li><li class="text-teal-600"><a><i class="icon-cog7"></i></a></li></ul></td></tr>`;
-        	    $("#tablaSistema").append(aux);
+                <li class="text-primary-600" data-toggle="modal" data-target="#newSystem"><a class="botonsaveEditar" name="sit" data-value="${entry.id}" data-id="1"><i  class="icon-pencil7" >
+                </i></a></li><li class="text-danger-600"><a data-value="${entry.id}" class="eliminarSistema"><i class="icon-trash"></i></a></li><li class="text-teal-600"><a><i class="icon-cog7"></i></a></li></ul></td></tr>`;
+                $("#tablaSistema").append(aux);
             }
-            /*para sacar el id del boton de la tabla*/
-             $('table a[name="sit"').off();
-             $('table a[name="sit"]').on('click',(ev)=>{
-            
-            let aux = ev.currentTarget
-             console.log($(aux).data("value"));
-        });
         })
-    }
+ }
 
-   setguardarSistemas(){	
-        let select_grupo = $( "#sel1 option:selected" ).val();  
-        var data={
-            nombre: $("#nom_sistema").val(),
-            descripcion:  $("#descripcion_sistema").val(),
-            sigla:'asa',
-            fecha_inicio:'01/01/2010',
-            fecha_fin:'01/01/2010',
+   setguardarSistemas(value : number, nombre : string, descripcion : string){	
+        let select_grupo = $( "#sel1 option:selected" ).val(); 
+        var data; 
+       if(nombre == "") {
+           data = null;
         }
-        console.log(data);
-        this.mantenimientosistemaService.saveSistema(data).done((resaponse) =>{
-            console.log(resaponse);
-        }).fail((response)=>{
-           console.log(response); 
-        });        
+        else{
+            data={
+                nombre: nombre,
+                descripcion:  descripcion,
+                sigla:'',
+                fecha_inicio:'',
+                fecha_fin:'',
+            }
+        }
+
+        if(data==null){
+           this.mantenimientosistemaService.saveSistema(data).done((resaponse) =>{
+                console.log(resaponse);
+            }).fail((response)=>{
+            console.log(response); 
+            }); 
+        }else{
+            console.log("aq");
+          this.mantenimientosistemaService.editSistema(value,data).done((resaponse) =>{
+                console.log(resaponse);
+            }).fail((response)=>{
+            console.log(response); 
+            });
+        }
     }
 
-    geteditarSistemas(id: number){
+ geteditarSistemas(id: number){
         this.mantenimientosistemaService.getSistemas().done((data)=>{
+            let aux = id;
             for(let entry of data){
-                let aux = entry.id
-                console.log(aux);
+                if(entry.id == aux)
+                    $("#nom_sistema").val(entry.nombre);
+                    $("#descripcion_sistema").val(entry.nombre);
             }
         })
-    }
+    }   
 
+    geteliminarSistema(id : number){
+        this.mantenimientosistemaService.deleteSistema(id).done((data)=>{
+                console.log(data);
+            }).fail((response)=>{
+            console.log(response); 
+        })
+    }
 } 
 
 new modulo_sistema()
